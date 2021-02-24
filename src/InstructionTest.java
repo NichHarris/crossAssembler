474c03857b25c766a9a2bf73320238c34c577e6f
lina <lina_t_135@hotmail.com>
2021-02-23 23:22:37 -0500

    just in case

--- src/InstructionTest.java
+++ src/InstructionTest.java
@@ -3,27 +3,27 @@ public class InstructionTest {
 
         //Testing the default constructor
         Instruction instruction1 = new Instruction();
-        testInstruction("Test Default Constructor", "null null", instruction1.getMnemonic()+" "+instruction1.getOperand());
+        testInstruction("Test -Instruction Class- Default Constructor", "null null", instruction1.getMnemonic()+" "+instruction1.getOperand());
 
         //Creating an instruction with both a Mnemonic and Operand
         instruction1 = new Instruction ("halt", "i3");
 
         //Testing the Constructor of the class with getOperand() and getMnemonic()
-        testInstruction("Test Mnemonic and Operand Constructor", "halt i3", instruction1.getMnemonic()+" "+instruction1.getOperand());
+        testInstruction("Test -Instruction Class- Mnemonic and Operand Constructor", "halt i3", instruction1.getMnemonic()+" "+instruction1.getOperand());
 
         //Testing setMnemonic(String m) and getMnemonic()
         instruction1.setMnemonic("not");
-        testInstruction("Test setMnemonic", "not", instruction1.getMnemonic());
+        testInstruction("Test -Instruction Class- setMnemonic", "not", instruction1.getMnemonic());
 
         //Testing setOperand(String o) and getOperand()
         instruction1.setOperand("12");
-        testInstruction("Test setOperand", "12", instruction1.getOperand());
+        testInstruction("Test -Instruction Class- setOperand", "12", instruction1.getOperand());
 
         //Testing the function getDigit() Instruction Class
-        testInstruction("Test isDigit", "true", Boolean.toString(instruction1.isDigit()));
+        testInstruction("Test -Instruction Class- isDigit", "true", Boolean.toString(instruction1.isDigit()));
 
         //Testing the function toString() from Instruction Class
-        testInstruction("Test toString", "'not 12'", instruction1.toString());
+        testInstruction("Test -Instruction Class- toString", "'not 12'", instruction1.toString());
 
     }
 
