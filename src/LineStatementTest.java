991787c499dc10723f69ff4ccfcfc9aa09949faf
lina <lina_t_135@hotmail.com>
2021-02-23 22:37:13 -0500

    made adjustments to LineStatementTest.java according to changes

--- src/LineStatementTest.java
+++ src/LineStatementTest.java
@@ -1,55 +1,39 @@
-/*public class LineStatementTest {
+public class LineStatementTest {
     public static void main(String[] args) throws Exception {
-        //Testing Default constructor
+        //1 Testing Default constructor
         LineStatement ls = new LineStatement();
-        TestLineStatement("Test Default Constructor", " null ", ls.getLabel() + " " + ls.getInstruction() + " " + ls.getComment());
+        TestLineStatement("Test Default Constructor", " 'null null' ", ls.getLabel() + " " + ls.getInstruction() + " " + ls.getComment());
 
-        //Testing Parametrized constructor for object initialization with label, instruction and comment
+        //2 Testing Parametrized constructor for object initialization with label, instruction and comment
         LineStatement ls1 = new LineStatement("Fct", new Instruction("ldc.i3","3"), "");
         TestLineStatement("Test Parameterized Constructor with label, instruction and comment", "Fct 'ldc.i3 3' ", ls1.getLabel() + " " + ls1.getInstruction().toString() + " " + ls1.getComment());
 
-        //Testing Parametrized constructor for object initialization with instruction and comment
-        LineStatement ls2 = new LineStatement(new Instruction("add", ""), "; This is a comment");
-        TestLineStatement("Test Parameterized Constructor with instruction and comment", "'add ' ; This is a comment", ls2.getInstruction().toString() + " " + ls2.getComment());
-
-        //Testing Parametrized constructor for object initialization with label and instruction
-        LineStatement ls3 = new LineStatement("Fct", new Instruction("ldc.i3","3"));
-        TestLineStatement("Test Parameterized Constructor with label and instruction", "Fct 'ldc.i3 3'", ls3.getLabel() + " " + ls3.getInstruction().toString());
-
-        //Testing Parametrized constructor for object initialization with label
-        LineStatement ls4 = new LineStatement("Fct");
-        TestLineStatement("Test Parameterized Constructor with a label", "Fct", ls4.getLabel());
-
-        //Testing Parametrized constructor for object initialization with label and comments
-        LineStatement ls5 = new LineStatement("Fct", "; this is another comment");
-        TestLineStatement("Test Parameterized Constructor with label and comments", "Fct ; this is another comment", ls5.getLabel() + " " + ls5.getComment());
-
         //Testing Setter for label
-        ls2.setLabel("RA");
-        TestLineStatement("Test setLabel()", "RA", ls2.getLabel());
+        ls1.setLabel("RA");
+        TestLineStatement("Test setLabel()", "RA", ls1.getLabel());
 
         //Testing Setter for instruction
-        ls2.setInstruction(new Instruction("pop", ""));
-        TestLineStatement("Test setInstruction()", "'pop '", ls2.getInstruction().toString());
+        ls1.setInstruction(new Instruction("pop", ""));
+        TestLineStatement("Test setInstruction()", "'pop '", ls1.getInstruction().toString());
 
         //Testing Setter for comments
-        ls2.setComment("; new comment");
-        TestLineStatement("Test setComment()", "; new comment", ls2.getComment());
+        ls1.setComment("; new comment");
+        TestLineStatement("Test setComment()", "; new comment", ls1.getComment());
 
         //Testing the function getLabel() from LineStatement Class
-        TestLineStatement("Test getLabel()","Fct", ls1.getLabel());
+        TestLineStatement("Test getLabel()","RA", ls1.getLabel());
 
         //Testing the function getInstruction() from LineStatement Class
-        TestLineStatement("Test getInstruction()", "'ldc.i3 3'", ls1.getInstruction().toString());
+        TestLineStatement("Test getInstruction()", "'pop '", ls1.getInstruction().toString());
 
         //Testing the function getComment() from LineStatement Class
-        TestLineStatement("Test getComment()", "", ls1.getComment());
+        TestLineStatement("Test getComment()", "; new comment", ls1.getComment());
 
         //Testing the function getCode() from LineStatement Class
-        TestLineStatement("Test getCode()","144", ls1.getCode().toString());
+        //TestLineStatement("Test getCode()","144", ls1.getCode().toString());
 
         //Testing the function toString() from LineStatement Class
-        TestLineStatement("Test toString()", "'Fct 'ldc.i3 3' '", ls1.toString());
+        TestLineStatement("Test toString()", "'RA 'pop ' ; new comment'", ls1.toString());
     }
 
     public static void TestLineStatement(String testCaseName, String expectedOutput, String methodOutput) throws Exception {
