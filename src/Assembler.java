adec68543d8d6e9dcce192632058f00cdedbd93f
NichHarris <harris.nicholas1998@gmail.com>
2021-04-11 20:14:52 -0400

    Change

--- src/Assembler.java
+++ src/Assembler.java
@@ -82,7 +82,7 @@ public class Assembler implements IAssembler{
         }
 
         //Update label table
-        for (int i = 0; i < interRep.getLength(); i++) {
+        for (int i = 0; i < interRep.getLength() - 1; i++) {
             //Add label in label field to label table
             if (!interRep.getLine(i).getLabel().equals("")) {
                 String label = interRep.getLine(i).getLabel();
